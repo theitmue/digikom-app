bb75d52acac27975eba021927bee50c2f7c21baa
tearaveler <thorsten.lucke@yahoo.de>
2021-07-13 16:01:50 +0200

    Update 2

--- src/Drawer.jsx
+++ src/Drawer.jsx
@@ -18,7 +18,7 @@ export default function NavDrawer(props) {
                     <IconButton onClick={handleDrawer} color = 'inherit' edge='start' aria-label='menu'>
                         <Menu />
                     </IconButton>
-                    <Typography variant="h6" >Digikom-App b0.3.2m</Typography>
+                    <Typography variant="h6" >Digikom-App b0.4.1</Typography>
                 </Toolbar>
             </AppBar>
             
